8f9ff2e014cdb7039390cb7afa910cbfaee2d10f
Phillip Whelan <pwhelan@exis.cl>
2017-12-11 18:23:30 -0300

    Cleanup the code.

--- src/sumo/vanity/main.go
+++ src/sumo/vanity/main.go
@@ -13,21 +13,57 @@ type keyPair struct {
 	Pub  *monero.Key
 }
 
-func worker(c chan keyPair, vanity string) {
+type wallet struct {
+	SpendKey *keyPair
+	ViewKey  *keyPair
+}
+
+func newKeyPair() *keyPair {
+	priv, pub := monero.NewKeyPair()
+	return &keyPair{Priv: priv, Pub: pub}
+}
+
+func worker(c chan *keyPair, vanity string) {
 	for {
-		priv, pub := monero.NewKeyPair()
-		pbuf := pub.ToBytes()
+		spend := newKeyPair()
+		pbuf := spend.Pub.ToBytes()
 		scratch := append(monero.Uint64ToBytes(0x2bb39a), pbuf[:]...)
 		slug := monero.EncodeMoneroBase58(scratch)
 		if slug[6:6+len(vanity)] == vanity {
-			c <- keyPair{priv, pub}
+			c <- spend
 		}
 	}
 }
 
+func (w *wallet) Address() string {
+	sbuf := w.SpendKey.Pub.ToBytes()
+	vbuf := w.ViewKey.Pub.ToBytes()
+	var buf []byte
+	buf = append(buf, monero.Uint64ToBytes(0x2bb39a)...)
+	buf = append(buf, sbuf[:]...)
+	buf = append(buf, vbuf[:]...)
+	scratch := append(monero.Uint64ToBytes(0x2bb39a), buf[:]...)
+	csum := monero.GetChecksum(scratch)
+	address := fmt.Sprintf("%s%s",
+		monero.EncodeMoneroBase58(buf[:]),
+		monero.EncodeMoneroBase58(csum[:4]))
+	return address
+}
+
+func (w *wallet) Print() {
+	spbuf := w.SpendKey.Priv.ToBytes()
+	vpbuf := w.ViewKey.Priv.ToBytes()
+	fmt.Printf("[!] Address: %s\n", w.Address())
+	fmt.Printf("[!] SpendKey: %s ViewKey: %s\n",
+		hex.EncodeToString(spbuf[:]),
+		hex.EncodeToString(vpbuf[:]))
+}
+
 func main() {
+	var w wallet
 	var threads int
 	var cores int
+
 	flag.IntVar(&threads, "threads", runtime.GOMAXPROCS(0),
 		"set the number of threads to use")
 	flag.IntVar(&cores, "cores", 0,
@@ -40,30 +76,12 @@ func main() {
 
 	fmt.Printf("[*] Threads: %d Cores: %d\n", threads, runtime.GOMAXPROCS(0))
 	fmt.Printf("[*] Searching for address starting with Sumoo#%s\n", flag.Arg(0))
-	c := make(chan keyPair)
+	c := make(chan *keyPair)
 	for i := 0; i < threads; i++ {
 		go worker(c, flag.Arg(0))
 	}
-	skeys := <-c
-	vpriv, vpub := monero.NewKeyPair()
-	sbuf := skeys.Pub.ToBytes()
-	vbuf := vpub.ToBytes()
-	spbuf := skeys.Priv.ToBytes()
-	vpbuf := vpriv.ToBytes()
-	var buf []byte
-	buf = append(buf, monero.Uint64ToBytes(0x2bb39a)...)
-	buf = append(buf, sbuf[:]...)
-	buf = append(buf, vbuf[:]...)
-	scratch := append(monero.Uint64ToBytes(0x2bb39a), buf[:]...)
-	csum := monero.GetChecksum(scratch)
-	address := fmt.Sprintf("%s%s",
-		monero.EncodeMoneroBase58(buf[:]),
-		monero.EncodeMoneroBase58(csum[:4]))
-	fmt.Printf("ADDRESS=%s\n", address)
-	fmt.Printf("SPENDKEY=%s VIEWKEY=%s\n",
-		hex.EncodeToString(spbuf[:]),
-		hex.EncodeToString(vpbuf[:]))
-	fmt.Printf("PUBLIC KEYS=%s / %s\n",
-		hex.EncodeToString(sbuf[:]),
-		hex.EncodeToString(vbuf[:]))
+
+	w.SpendKey = <-c
+	w.ViewKey = newKeyPair()
+	w.Print()
 }
